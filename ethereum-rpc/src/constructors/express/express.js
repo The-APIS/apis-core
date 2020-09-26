8e3276fad24700a51de239c7c1713d38b99f534a
Shain <srslafazan@gmail.com>
2020-09-26 19:16:20 -0300

    Implemented naive cache for ethereum-rpc

--- ethereum-rpc/src/constructors/express/express.js
+++ ethereum-rpc/src/constructors/express/express.js
@@ -3,13 +3,18 @@ const express = require('express')
 const cors = require('cors')
 const morgan = require('morgan')
 const bodyParser = require('body-parser')
+const apicache = require('apicache')
+
+
 
 module.exports = async (context) => {
   const app = express()
+  const cache = apicache.options({ redisClient: context.redis }).middleware
   app.disable('x-powered-by')
   app.use(morgan('combined'))
   app.use(bodyParser.json({ limit: '50mb', extended: true }))
   app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }))
-  app.use('/', require('./routes')(context))
+  app.use(cache('5 minutes'))
+  app.use('/', require('./routes')({ cache, ...context }))
   return app
 }
