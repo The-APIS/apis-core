8d1d1d5fc083d512bfa71bae475907af841e5771
Shain <srslafazan@gmail.com>
2020-12-19 23:46:39 -0800

    Moved to web3-providers-ws package for consistency

--- ethereum-listener/src/constructors/web3.js
+++ ethereum-listener/src/constructors/web3.js
@@ -1,13 +1,27 @@
 const Web3 = require('web3')
+const Web3WsProvider = require('web3-providers-ws');
 
-let web3
-let reconnectTimeout
+const options = {
+  timeout: 30000, // ms
 
-const Web3Provider = ({ address }) => {
-  const { HttpProvider } = Web3.providers
-  console.log(`[Web3][Websocket] Initializing connection... ${address}`)
-  return new Web3(new HttpProvider(address))
-}
+  // Useful for credentialed urls, e.g: ws://username:password@localhost:8546
+  // headers: {
+  //   authorization: `Basic ${process.env.ETHEREUM_WSS_USER}:${process.env.ETHEREUM_WSS_PASS}`
+  // },
 
+  // Useful if requests result are large
+  clientConfig: {
+    maxReceivedFrameSize: 100000000,   // bytes - default: 1MiB
+    maxReceivedMessageSize: 100000000, // bytes - default: 8MiB
+  },
 
-module.exports = Web3Provider({ address: process.env.ETHEREUM_HTTPS_ADDR || 'http://127.0.0.1:8546' })
+  // Enable auto reconnection
+  reconnect: {
+    auto: true,
+    delay: 5000, // ms
+    maxAttempts: 5,
+    onTimeout: false
+  },
+};
+
+module.exports = new Web3(new Web3WsProvider(process.env.ETHEREUM_WSS_ADDR || 'http://127.0.0.1:8546', options))
