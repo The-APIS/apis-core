c760adb09e586884329614c214f7208d9f6b9f48
santosh-shaastry <santosh.shaastry@lithium.com>
2021-05-18 17:40:16 +0530

    debug library already adds date/time in the console log. Removing the new library that I added

--- ethereum-listener/src/lib/syncBlocks.js
+++ ethereum-listener/src/lib/syncBlocks.js
@@ -1,6 +1,5 @@
 const syncBlock = require('./syncBlock')
 const { getDate } = require("/app/share/lib")
-const date = require("date-and-time")
 const debug = require('debug')('ethereum-listener:lib:syncBlocks')
 const getSyncStartAndEndBlocks = require('./getSyncStartAndEndBlocks')
 
@@ -18,14 +17,14 @@ module.exports = async ({
     let end = startBlockNumber + process.env.BLOCK_STEP_COUNT;
 
     for ( current = startBlockNumber; current < end; current++) {
-      debug(`[${date.format(new Date(), 'YYYY/MM/DD HH:mm:ss')}] START syncing block ${current}`)
+      debug(`START syncing block ${current}`)
       await syncBlock({
         blockNumber: current,
         withMethods: true,
         web3,
         models  
       })
-      debug(`[${date.format(new Date(), 'YYYY/MM/DD HH:mm:ss')}] END syncing block ${current}`)
+      debug(`END syncing block ${current}`)
     }
 
   } catch (e) {
