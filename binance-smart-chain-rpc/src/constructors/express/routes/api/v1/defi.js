0a751e63b644a561667cf9070ab3d8b3aa30f60b
Ajmal <ajmalbinahamed@gmail.com>
2021-06-28 18:22:49 +0530

    removed bsc-rpc local code

--- binance-smart-chain-rpc/src/constructors/express/routes/api/v1/defi.js
+++
@@ -1,7 +0,0 @@
-const axios = require('axios')
-const router = require('express').Router()
-
-
-module.exports = ({ models, ...context }) => {
-  return router
-}
--- binance-smart-chain-rpc/src/constructors/express/routes/api/v1/methods.js
+++
@@ -1,6 +0,0 @@
-const axios = require('axios')
-const router = require('express').Router()
-
-module.exports = ({ models, sequelize, ...context }) => {
-  return router
-}
