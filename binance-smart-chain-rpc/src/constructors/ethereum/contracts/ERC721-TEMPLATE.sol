aae30b0ea4ffb7b1c4237e0a410967692a9e160a
Ajmal <ajmalbinahamed@gmail.com>
2021-07-12 10:39:27 +0530

    removed binance-smart-chain-rpc-code

--- binance-smart-chain-rpc/src/constructors/ethereum/contracts/ERC721-TEMPLATE.sol
+++
@@ -1,7 +0,0 @@
-pragma solidity ^0.6.0;
-
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-
-contract {{token.name}} is ERC721 {
-  constructor() ERC721("{{token.name}}", "{{token.symbol}}") public {}
-}
