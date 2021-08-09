04b59d0537ffcb13bd6d88c3d52e851c529d07ee
AjmalAhamedKodakkat <ajmalbinahamed@gmail.com>
2021-08-09 17:00:13 +0530

    added mainnet functionality to pair and price routes

--- share/constants/index.js
+++ share/constants/index.js
@@ -10,3 +10,9 @@ module.exports.SUPPORTED_NETWORKS = [
   'mainnet',
   'testnet',
 ]
+
+module.exports.UNISWAP_SUPPORTED_CHAIN = ['ethereum']
+
+module.exports.UNISWAP_SUPPORTED_NETWORK = ['mainnet','rinkeby']
+
+module.exports.UNISWAP_ROUTER_ADDRESS = "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D"
